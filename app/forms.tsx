a2ea7fd23c95eae5382b2146d5907c91f87e2c44
alstjd0051 <wsc03002@naver.com>
2023-03-19 23:32:57 +0900

    UPDATE:: Less code,Easier Inputs

--- app/forms.tsx
+++ app/forms.tsx
@@ -1,67 +1,24 @@
 "use client";
 
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// Less code
+// Better validation
+// Better Errors(set,clear,display)
+// Have contorl over inputs
+// Dont deal with evetnts
+// Easier Inputs
 
 type Props = {};
 
 const Forms = (props: Props) => {
-  const [username, setusername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formError, setFormError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const onUsernameChage = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setusername(value);
-  };
-  const onEmaileChage = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setEmailError("");
-
-    setEmail(value);
-  };
-  const onPasswordChage = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setPassword(value);
-  };
-
-  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (username === "" || email === "" || password === "") {
-      setFormError("All fileds are required");
-    }
-    if (!email.includes("@")) {
-      setEmailError("email is required");
-    }
-    console.log(username, email, password);
-  };
+  const { register, watch } = useForm();
   return (
-    <form onSubmit={onSubmit} className="flex flex-col">
-      <input
-        value={username}
-        onChange={onUsernameChage}
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        onChange={onEmaileChage}
-        value={email}
-        type="email"
-        placeholder="Email"
-      />
-      {emailError}
-      <input
-        onChange={onPasswordChage}
-        value={password}
-        type="password"
-        placeholder="Password"
-      />
+    <form className="flex flex-col">
+      <input {...register("username")} type="text" placeholder="Username" />
+      <input {...register("email")} type="email" placeholder="Email" />
+      <input {...register("password")} type="password" placeholder="Password" />
       <input type="submit" value="Create Account" />
     </form>
   );

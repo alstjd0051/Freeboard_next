40674806d6cd650b17cd8800dd43d82d03f97787
alstjd0051 <wsc03002@naver.com>
2023-03-19 18:44:59 +0900

    NextJS& Tailwind Setting

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const head = () => {
+  return (
+    <>
+      <title>Next13</title>
+    </>
+  );
+};
+
+export default head;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Forms from "./forms";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Forms />
+    </main>
+  );
+}

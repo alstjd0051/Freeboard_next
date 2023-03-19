dceb15cb8f4cf4661562360188beb6ba731a1268
alstjd0051 <wsc03002@naver.com>
2023-03-20 00:40:39 +0900

    UPDATE::  add interface, Valid, useForm

--- app/forms.tsx
+++ app/forms.tsx
@@ -1,36 +1,52 @@
 "use client";
 
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
 // Better validation
 // Better Errors(set,clear,display)
 // Have contorl over inputs
 
-type Props = {};
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 const Forms = () => {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
     console.log("Hello");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
   return (
     <form
-      onSubmit={handleSubmit(onValid)}
+      onSubmit={handleSubmit(onValid, onInvalid)}
       className="flex flex-col w-80 m-auto py-5 gap-2 border bg-gray-400 space-y-5  "
     >
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "user is required",
+          minLength: {
+            message: "the username shold be longer than 5 chars",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", { required: "email is required", minLength: 5 })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...register("password", { required: true })}
+        {...register("password", {
+          required: "password is required",
+          minLength: 8,
+        })}
         type="password"
         placeholder="Password"
       />

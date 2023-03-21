5dd2dfcd596424f66de5d58256a7f4e81f210407
alstjd0051 <wsc03002@naver.com>
2023-03-21 12:14:14 +0900

    update:: publish

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="p-5 bg-blue-500">
+    <header className="p-5 bg-blue-500 flex items-center gap-5">
       <Link href={"/"} className="px-2 py-1 bg-white text-blue-500 rounded-lg">
         Home
       </Link>
@@ -14,6 +14,12 @@ const Header = (props: Props) => {
         className="px-2 py-1 bg-white text-blue-500 rounded-lg"
       >
         Todos
+      </Link>
+      <Link
+        href={"/search"}
+        className="px-2 py-1 bg-white text-blue-500 rounded-lg"
+      >
+        Search
       </Link>
     </header>
   );
--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const head = () => {
   return (
     <>
-      <title>Next13</title>
+      <title>TylerSong - website</title>
     </>
   );
 };

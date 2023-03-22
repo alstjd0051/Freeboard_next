6ebd1d9fc92decd540aaaa6ac5682a8ce70ff570
alstjd0051 <wsc03002@naver.com>
2023-03-22 11:01:32 +0900

    update:: reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import { Suspense } from "react";
-import TodosList from "./(users)/todos/TodoList";
-
 export default function Home() {
   return (
     <div className="flex flex-col space-y-10">
-      <Suspense fallback={<p className="text-red-500">Loading the Todos</p>}>
-        <div className="flex  space-x-2 ">
-          {/* @ts-ignore */}
-          <TodosList />
-        </div>
-      </Suspense>
-      <Suspense
-        fallback={<p className="text-blue-500">Loading the Shopping Trolley</p>}
-      >
-        <div className="flex space-x-2">
-          {/* @ts-ignore */}
-          <TodosList />
-        </div>
-      </Suspense>
+      <div></div>
     </div>
   );
 }

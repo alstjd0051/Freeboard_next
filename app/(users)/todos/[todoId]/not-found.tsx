5bf847dc6bed3ccdba48094869262293479e4d48
alstjd0051 <wsc03002@naver.com>
2023-03-22 11:01:03 +0900

    update:: reset

--- app/(users)/todos/[todoId]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const NotFound = (props: Props) => {
-  return <div>Whoops we couldnt find the TODO you are looking for</div>;
-};
-
-export default NotFound;

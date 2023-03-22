286a6c9808788c22c956cd24c68815d3054398c4
alstjd0051 <wsc03002@naver.com>
2023-03-22 13:15:26 +0900

    update:: validation check error

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -10,53 +10,79 @@ interface LoginForm {
 }
 
 const FormPage = () => {
-  const { register, handleSubmit, watch } = useForm<LoginForm>({
-    defaultValues: {},
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
   });
   const onValid = (data: LoginForm) => {};
   const onInvalid = (error: FieldErrors) => {
     console.log(error);
   };
+  console.log(errors);
   return (
     <div className="mx-auto py-10 ">
       <form
         onSubmit={handleSubmit(onValid, onInvalid)}
         className="flex flex-col items-center space-y-5  "
       >
-        <input
-          {...register("username", {
-            required: "username is required",
-            minLength: {
-              message: "3글자이상 입력해주세요",
-              value: 3,
-            },
-          })}
-          type="text"
-          placeholder="UserName"
-        />{" "}
-        <input
-          {...register("email", {
-            required: "email is required",
-            validate: {
-              notGmail: (value) =>
-                (value.includes(".com") || "mail을 입력해주세요") &&
-                (!value.includes("gmail.com") || "Gmail은 입력안됩니다."),
-            },
-          })}
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          {...register("password", {
-            required: "password is required",
-            minLength: {
-              message: "너무짧습니다 최소 8글자이상",
-              value: 8,
-            },
-          })}
-          type="password"
-          placeholder="Password"
-        />
+        <div className="relative flex space-x-60 ">
+          <input
+            {...register("username", {
+              required: "username is required",
+              minLength: {
+                message: "3글자이상 입력해주세요",
+                value: 3,
+              },
+            })}
+            type="text"
+            placeholder="UserName"
+            className="p-5 outline-none border-2 rounded-lg"
+          />
+          <span className="absolute flex-none w-full top-5  ">
+            {errors.username?.message}
+          </span>
+        </div>
+        <div className="relative flex space-x-60">
+          <input
+            {...register("email", {
+              required: "이메일을 입력해주세요",
+              validate: {
+                notGmail: (value) =>
+                  ((!value.includes("@gmail") || "Gemail을 입력해주세요") &&
+                    !value.includes("@naver")) ||
+                  "네이버메일은 안됩니다.",
+              },
+            })}
+            type="email"
+            placeholder="Email"
+            className={`p-5 outline-none border-2 rounded-lg ${
+              Boolean(errors.email?.message) ? "border-red-500" : ""
+            }`}
+          />
+          <span className="absolute flex-none w-full top-5  ">
+            {errors.email?.message}
+          </span>
+        </div>
+        <div className="relative flex space-x-60">
+          <input
+            {...register("password", {
+              required: "password is required",
+              minLength: {
+                message: "너무짧습니다 최소 8글자이상",
+                value: 8,
+              },
+            })}
+            type="password"
+            placeholder="Password"
+            className="p-5 outline-none border-2 rounded-lg"
+          />
+          <span className="absolute flex-none w-full top-5  ">
+            {errors.password?.message}
+          </span>
+        </div>
         {/* <input type="submit" value="Create Account" /> */}
         <input
           className="cursor-pointer"

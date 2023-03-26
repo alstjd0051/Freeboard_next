16a04b16bee6dde055f12cbf4cb80fab5f0517f4
alstjd0051 <wsc03002@naver.com>
2023-03-26 18:45:21 +0900

    UPDATE:: Connect Twilio, sendgrid

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,59 +1,59 @@
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@/components/commons/libs/server/withHandler";
+import withHandler, {
+  ResponseType,
+} from "@/components/commons/libs/server/withHandler";
 import { client } from "@/components/commons/libs/server/client";
+import { isAwaitKeyword } from "typescript";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-
-  console.log(user);
-  /* if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("founded");
-    if (!user) {
-      console.log("Did not found will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymouse",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
   if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your Login token is ${payload}`,
     });
-    if (user) console.log("founded");
-    if (!user) {
-      console.log("Did not found will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymouse",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
-  return res.status(200).end();
+    console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "wsc7202@gmail.com",
+      to: "wsc7202@gmail.com",
+      subject: "당신의 이메일을 확인하시겠습니까?",
+      text: `Your Token is ${payload}`,
+      html: `<strong>Your Token is ${payload}</strong>`,
+    });
+    console.log(email);
+  }
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);

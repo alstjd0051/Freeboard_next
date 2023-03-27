0927e2db041bf9dfeb3489bd4d99a22b30aa0925
alstjd0051 <wsc03002@naver.com>
2023-03-27 23:04:00 +0900

    UPDATE:: refactoring to  useUser

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import "./globals.css";
+import { SWRConfig } from "swr";
 
 export default function RootLayout({
   children,
@@ -7,7 +10,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <SWRConfig
+          value={{
+            refreshInterval: 5000,
+            fetcher: (url: string) =>
+              fetch(url).then((response) => response.json()),
+          }}
+        >
+          {children}
+        </SWRConfig>
+      </body>
     </html>
   );
 }

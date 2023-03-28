6d931e77b0a628ad6623fdadff2ddd2e8af0cb89
alstjd0051 <wsc03002@naver.com>
2023-03-28 17:33:36 +0900

    UPDATE::interface&useEffect

--- app/products/upload/page.tsx
+++ app/products/upload/page.tsx
@@ -4,7 +4,10 @@ import Input from "@/components/commons/items/input";
 import TextArea from "@/components/commons/items/textarea";
 import Layout from "@/components/commons/layout";
 import useMutation from "@/components/commons/libs/client/useMutation";
+import { Product } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { TbPhotoPlus } from "react-icons/tb";
 
@@ -13,14 +16,25 @@ interface UploadproductForm {
   price: number;
   description: string;
 }
+interface UploadProductMutation {
+  ok: boolean;
+  product: Product;
+}
 
 const Upload: NextPage = () => {
+  const router = useRouter();
   const { register, handleSubmit } = useForm<UploadproductForm>();
-  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+  const [uploadProduct, { loading, data }] =
+    useMutation<UploadProductMutation>("/api/products");
   const onValid = (data: UploadproductForm) => {
     if (loading) return;
-    console.log(uploadProduct);
+    uploadProduct(data);
   };
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/products/${data.product.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack hasTabBar title="Upload Product">
       <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
@@ -29,8 +43,8 @@ const Upload: NextPage = () => {
             htmlFor=""
             className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed bg-gray-300 h-48 rounded-md "
           >
-            <TbPhotoPlus className="h-12 w-12 stroke-current fill-none " />
             <input className="hidden" type="file" />
+            <TbPhotoPlus className="h-12 w-12 stroke-current fill-none " />
           </label>
         </div>
         <Input

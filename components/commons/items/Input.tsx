20ff82ad71f2de6555dc878656310730b2bde72d
alstjd0051 <wsc03002@naver.com>
2023-04-03 00:50:54 +0900

    airbnb-yt

--- components/commons/items/Input.tsx
+++ components/commons/items/Input.tsx
@@ -1,7 +1,38 @@
-### Animation
+"use client";
+import React from "react";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
 
-```jsx
-<input
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  errors,
+  label,
+  register,
+  disabled,
+  formatPrice,
+  required,
+  type = "text",
+}) => {
+  return (
+    <div className="w-full relative ">
+      {formatPrice && (
+        <BiDollar
+          size={24}
+          className="text-neutral-700 absolute top-5 left-2 "
+        />
+      )}
+      <input
         id={id}
         disabled={disabled}
         {...register(id, { required })}
@@ -39,4 +70,8 @@
       >
         {label}
       </label>
-```
+    </div>
+  );
+};
+
+export default Input;
